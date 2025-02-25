e3810b00b158c30b8c27c96744319a78c60bdfaa
SANDY13061 <113618860+SANDY13061@users.noreply.github.com>
2025-02-25 19:46:40 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Ensure dark mode is enabled
+
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
